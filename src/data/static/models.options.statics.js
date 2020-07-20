d15acccb1215887650416144ea3a6afc37c84796
Juan Panasiti <juanpanasiti@gmail.com>
2020-07-20 19:37:19 -0300

    Cambio cc por ml

--- src/data/static/models.options.statics.js
+++ src/data/static/models.options.statics.js
@@ -1,6 +1,6 @@
 module.exports = {
     unidadesMedida: () => {
-        options = ["gr", "cc", "unidades"]
+        options = ["gr", "ml", "u."]
         return options
     },
     estadoPedido: () => {
